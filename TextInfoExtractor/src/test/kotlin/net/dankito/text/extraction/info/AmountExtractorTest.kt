8a967834809093efeba635d6a82a10ba67179b4f
dankl <dankito@users.noreply.github.com>
2020-01-08 15:03:47 +0100

    Using now JUnit 5 parameters for test definitions

--- TextInfoExtractor/src/test/kotlin/net/dankito/text/extraction/info/AmountExtractorTest.kt
+++ TextInfoExtractor/src/test/kotlin/net/dankito/text/extraction/info/AmountExtractorTest.kt
@@ -2,298 +2,101 @@ package net.dankito.text.extraction.info
 
 import net.dankito.text.extraction.info.model.AmountOfMoney
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
 
 class AmountExtractorTest {
 
+    companion object {
+        private val CurrencySymbols = listOf("$", "€", "EUR", "£") // cannot be made a compile-time constant in Kotlin so that @ValueSource does not work
+
+        @JvmStatic // must be static for JUnit / JVM
+        fun currenciesProvider() = CurrencySymbols // therefore we supply a method as provider
+    }
+
+
     private val underTest = AmountExtractor()
 
 
-    @Test
-    fun `Integer € symbol`() {
+    @ParameterizedTest
+    @MethodSource("currenciesProvider")
+    fun `Integer`(currency: String) {
 
         // when
-        val result = underTest.extractAmountsOfMoney(listOf("10 €"))
+        val result = underTest.extractAmountsOfMoney(listOf("10 $currency"))
 
         // then
-        assertAmountAndCurrency(result, 10.0, "€")
-    }
-
-    @Test
-    fun `Integer EUR symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("10 EUR"))
-
-        // then
-        assertAmountAndCurrency(result, 10.0, "EUR")
-    }
-
-    @Test
-    fun `Integer $ symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("10 $"))
-
-        // then
-        assertAmountAndCurrency(result, 10.0, "$")
-    }
-
-    @Test
-    fun `Integer £ symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("10 £"))
-
-        // then
-        assertAmountAndCurrency(result, 10.0, "£")
+        assertAmountAndCurrency(result, 10.0, currency)
     }
 
 
-    @Test
-    fun `Dot as decimal separator € symbol`() {
+    @ParameterizedTest
+    @MethodSource("currenciesProvider")
+    fun `Dot as decimal separator`(currency: String) {
 
         // when
-        val result = underTest.extractAmountsOfMoney(listOf("10.51 €"))
+        val result = underTest.extractAmountsOfMoney(listOf("10.51 $currency"))
 
         // then
-        assertAmountAndCurrency(result, 10.51, "€")
+        assertAmountAndCurrency(result, 10.51, currency)
     }
 
-    @Test
-    fun `Dot as decimal separator EUR symbol`() {
+    @ParameterizedTest
+    @MethodSource("currenciesProvider")
+    fun `Comma as decimal separator`(currency: String) {
 
         // when
-        val result = underTest.extractAmountsOfMoney(listOf("10.51 EUR"))
+        val result = underTest.extractAmountsOfMoney(listOf("10,51 $currency"))
 
         // then
-        assertAmountAndCurrency(result, 10.51, "EUR")
-    }
-
-    @Test
-    fun `Dot as decimal separator $ symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("10.51 $"))
-
-        // then
-        assertAmountAndCurrency(result, 10.51, "$")
-    }
-
-    @Test
-    fun `Dot as decimal separator £ symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("10.51 £"))
-
-        // then
-        assertAmountAndCurrency(result, 10.51, "£")
+        assertAmountAndCurrency(result, 10.51, currency)
     }
 
 
-    @Test
-    fun `Comma as decimal separator € symbol`() {
+    @ParameterizedTest
+    @MethodSource("currenciesProvider")
+    fun `Comma as thousand separator`(currency: String) {
 
         // when
-        val result = underTest.extractAmountsOfMoney(listOf("10,51 €"))
+        val result = underTest.extractAmountsOfMoney(listOf("1,234,567 $currency"))
 
         // then
-        assertAmountAndCurrency(result, 10.51, "€")
+        assertAmountAndCurrency(result, 1234567.0, currency)
     }
 
-    @Test
-    fun `Comma as decimal separator EUR symbol`() {
+    @ParameterizedTest
+    @MethodSource("currenciesProvider")
+    fun `Dot as thousand separator`(currency: String) {
 
         // when
-        val result = underTest.extractAmountsOfMoney(listOf("10,51 EUR"))
+        val result = underTest.extractAmountsOfMoney(listOf("1.234.567 $currency"))
 
         // then
-        assertAmountAndCurrency(result, 10.51, "EUR")
-    }
-
-    @Test
-    fun `Comma as decimal separator $ symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("10,51 $"))
-
-        // then
-        assertAmountAndCurrency(result, 10.51, "$")
-    }
-
-    @Test
-    fun `Comma as decimal separator £ symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("10,51 £"))
-
-        // then
-        assertAmountAndCurrency(result, 10.51, "£")
+        assertAmountAndCurrency(result, 1234567.0, currency)
     }
 
 
-    @Test
-    fun `Comma as thousand separator € symbol`() {
+    @ParameterizedTest
+    @MethodSource("currenciesProvider")
+    fun `Comma as thousand separator and dot as decimal separator`(currency: String) {
 
         // when
-        val result = underTest.extractAmountsOfMoney(listOf("1,234,567 €"))
+        val result = underTest.extractAmountsOfMoney(listOf("1,234,567.89 $currency"))
 
         // then
-        assertAmountAndCurrency(result, 1234567.0, "€")
+        assertAmountAndCurrency(result, 1234567.89, currency)
     }
 
-    @Test
-    fun `Comma as thousand separator EUR symbol`() {
+    @ParameterizedTest
+    @MethodSource("currenciesProvider")
+    fun `Dot as thousand separator and comma as decimal separator`(currency: String) {
 
         // when
-        val result = underTest.extractAmountsOfMoney(listOf("1,234,567 EUR"))
+        val result = underTest.extractAmountsOfMoney(listOf("1.234.567,89 $currency"))
 
         // then
-        assertAmountAndCurrency(result, 1234567.0, "EUR")
-    }
-
-    @Test
-    fun `Comma as thousand separator $ symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("1,234,567 $"))
-
-        // then
-        assertAmountAndCurrency(result, 1234567.0, "$")
-    }
-
-    @Test
-    fun `Comma as thousand separator £ symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("1,234,567 £"))
-
-        // then
-        assertAmountAndCurrency(result, 1234567.0, "£")
-    }
-
-
-    @Test
-    fun `Dot as thousand separator € symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("1.234.567 €"))
-
-        // then
-        assertAmountAndCurrency(result, 1234567.0, "€")
-    }
-
-    @Test
-    fun `Dot as thousand separator EUR symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("1.234.567 EUR"))
-
-        // then
-        assertAmountAndCurrency(result, 1234567.0, "EUR")
-    }
-
-    @Test
-    fun `Dot as thousand separator $ symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("1.234.567 $"))
-
-        // then
-        assertAmountAndCurrency(result, 1234567.0, "$")
-    }
-
-    @Test
-    fun `Dot as thousand separator £ symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("1.234.567 £"))
-
-        // then
-        assertAmountAndCurrency(result, 1234567.0, "£")
-    }
-
-
-    @Test
-    fun `Comma as thousand separator and dot as decimal separator € symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("1,234,567.89 €"))
-
-        // then
-        assertAmountAndCurrency(result, 1234567.89, "€")
-    }
-
-    @Test
-    fun `Comma as thousand separator and dot as decimal separator EUR symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("1,234,567.89 EUR"))
-
-        // then
-        assertAmountAndCurrency(result, 1234567.89, "EUR")
-    }
-
-    @Test
-    fun `Comma as thousand separator and dot as decimal separator $ symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("1,234,567.89 $"))
-
-        // then
-        assertAmountAndCurrency(result, 1234567.89, "$")
-    }
-
-    @Test
-    fun `Comma as thousand separator and dot as decimal separator £ symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("1,234,567.89 £"))
-
-        // then
-        assertAmountAndCurrency(result, 1234567.89, "£")
-    }
-
-
-    @Test
-    fun `Dot as thousand separator and comma as decimal separator € symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("1.234.567,89 €"))
-
-        // then
-        assertAmountAndCurrency(result, 1234567.89, "€")
-    }
-
-    @Test
-    fun `Dot as thousand separator and comma as decimal separator EUR symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("1.234.567,89 EUR"))
-
-        // then
-        assertAmountAndCurrency(result, 1234567.89, "EUR")
-    }
-
-    @Test
-    fun `Dot as thousand separator and comma as decimal separator $ symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("1.234.567,89 $"))
-
-        // then
-        assertAmountAndCurrency(result, 1234567.89, "$")
-    }
-
-    @Test
-    fun `Dot as thousand separator and comma as decimal separator £ symbol`() {
-
-        // when
-        val result = underTest.extractAmountsOfMoney(listOf("1.234.567,89 £"))
-
-        // then
-        assertAmountAndCurrency(result, 1234567.89, "£")
+        assertAmountAndCurrency(result, 1234567.89, currency)
     }
 
 
