1429bdc3aca004f122730ed6057a3ef93b4e7567
dankl <dankito@users.noreply.github.com>
2020-04-10 21:43:38 +0200

    Moved TextInfoExtractor to an extra project

--- TextInfoExtractor/src/main/kotlin/net/dankito/text/extraction/info/model/BankData.kt
+++
@@ -1,4 +0,0 @@
-package net.dankito.text.extraction.info.model
-
-
-open class BankData(val ibans: List<StringSearchResult>, val bics: List<StringSearchResult>)
--- TextInfoExtractor/src/main/kotlin/net/dankito/text/extraction/info/model/SearchResult.kt
+++
@@ -1,5 +0,0 @@
-package net.dankito.text.extraction.info.model
-
-
-open class SearchResult(val foundInLine: String) {
-}
